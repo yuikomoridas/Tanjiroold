72e42f7a4179486614cb95fecad3f0860991f661
Tanjiro Kamado <69890535+JayPatel1314@users.noreply.github.com>
2020-12-20 19:40:43 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -198,7 +198,7 @@ else:
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("saitama", API_ID, API_HASH)
-pgram = Client("AshPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+pgram = Client("TanjiroPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
