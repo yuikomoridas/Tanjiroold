9c7d875800459d11821ad52c81ca84a61fd83210
Tanjiro Kamado <69890535+JayPatel1314@users.noreply.github.com>
2020-12-30 10:47:53 +0530

    Update speed_test.py

--- SaitamaRobot/modules/speed_test.py
+++ SaitamaRobot/modules/speed_test.py
@@ -47,7 +47,7 @@ def speedtestxyz_callback(update: Update, context: CallbackContext):
                 replymsg, parse_mode=ParseMode.MARKDOWN)
     else:
         query.answer(
-            "You are required to join Heroes Association to use this command.")
+            "You are required to join Demon Slayer Bureau to use this command.")
 
 
 SPEED_TEST_HANDLER = DisableAbleCommandHandler("speedtest", speedtestxyz)
