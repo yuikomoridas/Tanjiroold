762d0137245b3dc168a2adb49a273e080a4ab231
Tanjiro Kamado <69890535+JayPatel1314@users.noreply.github.com>
2020-12-31 10:31:23 +0530

    Update myanimelist.py

--- SaitamaRobot/modules/myanimelist.py
+++ SaitamaRobot/modules/myanimelist.py
@@ -1,144 +1,206 @@
 # Module to get info about anime, characters, manga etc. by @TheRealPhoenix
 
-from typing import List
-from malclient import Client
-from malclient.exceptions import APIException
+from jikanpy import Jikan
+from jikanpy.exceptions import APIException
 
-from telegram import Bot, Update, Message, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import run_async
-from tg_bot import OWNER_ID, MAL_CLIENT_ID, MAL_ACCESS_TOKEN, MAL_REFRESH_TOKEN, dispatcher
-from tg_bot.modules.disable import DisableAbleCommandHandler
+from telegram import Message, Chat, User, ParseMode, Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 
+from SaitamaRobot import dispatcher
 
-client = Client()
-client.init(access_token=MAL_ACCESS_TOKEN)
-
-
-def refresh_token(msg: Message, error: APIException) -> None:
-    if str(error.response) == "<Response [401]>":
-        client.refresh_bearer_token(
-            client_id=MAL_CLIENT_ID,
-            refresh_token=MAL_REFRESH_TOKEN,
-            client_secret=None
-        )
-        new_access_token = client.bearer_token
-        new_refresh_token = client.refresh_token
-        MSG_TEXT = (f"Your MAL access token has expired.\n*New Access Token*: `{new_access_token}`\n"
-            f"*New Refresh Token*: `{new_refresh_token}`")
-        bot.send_message(OWNER_ID, MSG_TEXT, parse_mode="MARKDOWN")
-    else:
-        msg.reply_text(f"An error occurred:\n`{error}`", parse_mode="MARKDOWN")
+jikan = Jikan()
 
 
 @run_async
-def search_anime(bot: Bot, update: Update, args: List[str]) -> None:
+def anime(update: Update, context: CallbackContext):
     msg = update.effective_message
+    args = context.args
     query = " ".join(args)
-    if not query:
-        msg.reply_text("I can't search for nothing...")
-        return
+    res = ""
     try:
-        anime = client.search_anime(query)
-    except APIException as e:
-        refresh_token(msg, e)
-    if not anime:
-        msg.reply_text("Not found!")
-        return
-    anime_id = anime[0].id
-    res = client.get_anime_details(anime_id)
-    if res.status == "finished_airing":
-        status = "Finished Airing"
-        episodes = res.num_episodes
+        res = jikan.search("anime", query)
+    except APIException:
+        msg.reply_text("Error connecting to the API. Please try again!")
+        return ""
+    try:
+        res = res.get("results")[0].get("mal_id") # Grab first result
+    except APIException:
+        msg.reply_text("Error connecting to the API. Please try again!")
+        return ""
+    if res:
+        anime = jikan.anime(res)
+        title = anime.get("title")
+        japanese = anime.get("title_japanese")
+        type = anime.get("type")
+        duration = anime.get("duration")
+        synopsis = anime.get("synopsis")
+        source = anime.get("source")
+        status = anime.get("status")
+        episodes = anime.get("episodes")
+        score = anime.get("score")
+        rating = anime.get("rating")
+        genre_lst = anime.get("genres")
+        genres = ""
+        for genre in genre_lst:
+            genres += genre.get("name") + ", "
+        genres = genres[:-2]
+        studios = ""
+        studio_lst = anime.get("studios")
+        for studio in studio_lst:
+            studios += studio.get("name") + ", "
+        studios = studios[:-2]
+        duration = anime.get("duration")
+        premiered = anime.get("premiered")
+        image_url = anime.get("image_url")
+        url = anime.get("url")
+        trailer = anime.get("trailer_url")
     else:
-        episodes = None
-    genres_list = []
-    for i in res.genres:
-        genres_list.append(i.name)
-    genres = ", ".join(genres_list)
-    studio_list = []
-    for i in res.studios:
-        studio_list.append(i.name)
-    studios = ", ".join(studio_list)
-    if res.status == "currently_airing":
-        status = "Currently Airing"
-    if res.start_season:
-        premier = res.start_season
-    premiered = f"{premier.year} {premier.season.capitalize()}"
-    image = res.main_picture.large
-    text = f"<b>{res.title} ({res.alternative_titles.ja})</b>\n"
-    text += f"<b>Type</b>: <code>{res.media_type.upper()}</code>\n"
-    text += f"<b>Source</b>: <code>{res.source.replace('_', ' ').capitalize()}</code>\n"
-    text += f"<b>Status</b>: <code>{status}</code>\n"
-    text += f"<b>Genres</b>: <code>{genres}</code>\n"
-    if episodes:
-        text += f"<b>Episodes</b>: <code>{episodes}</code>\n"
-    text += f"<b>Score</b>: <code>{res.mean}</code>\n"
-    text += f"<b>Ranked</b>: <code>#{res.rank}</code>\n"
-    text += f"<b>Studio(s)</b>: <code>{studios}</code>\n"
-    text += f"<b>Premiered</b>: <code>{premiered}</code>\n\n"
-    text += f"<a href='{image}'>\u200c</a>"
-    text += res.synopsis
-    keyb = [
-        [InlineKeyboardButton("More Information", url=f"https://myanimelist.net/anime/{anime_id}")]
-    ]
+        msg.reply_text("No results found!")
+        return
+    rep = f"<b>{title} ({japanese})</b>\n"
+    rep += f"<b>Type:</b> <code>{type}</code>\n"
+    rep += f"<b>Source:</b> <code>{source}</code>\n"
+    rep += f"<b>Status:</b> <code>{status}</code>\n"
+    rep += f"<b>Genres:</b> <code>{genres}</code>\n"
+    rep += f"<b>Episodes:</b> <code>{episodes}</code>\n"
+    rep += f"<b>Duration:</b> <code>{duration}</code>\n"
+    rep += f"<b>Score:</b> <code>{score}</code>\n"
+    rep += f"<b>Studio(s):</b> <code>{studios}</code>\n"
+    rep += f"<b>Premiered:</b> <code>{premiered}</code>\n"
+    rep += f"<b>Rating:</b> <code>{rating}</code>\n\n"
+    rep += f"<a href='{image_url}'>\u200c</a>"
+    rep += f"<i>{synopsis}</i>\n"
+    if trailer:
+        keyb = [
+            [InlineKeyboardButton("More Information", url=url),
+           InlineKeyboardButton("Trailer", url=trailer)]
+        ]
+    else:
+        keyb = [
+             [InlineKeyboardButton("More Information", url=url)]
+         ]
     
-    msg.reply_text(text, parse_mode="HTML", reply_markup=InlineKeyboardMarkup(keyb))
-
+    
+    msg.reply_text(rep, parse_mode=ParseMode.HTML, reply_markup=InlineKeyboardMarkup(keyb))
+    
 
 @run_async
-def search_manga(bot: Bot, update: Update, args: List[str]) -> None:
+def character(update: Update, context: CallbackContext):
     msg = update.effective_message
+    res = ""
+    args = context.args
     query = " ".join(args)
-    if not query:
-        msg.reply_text("I can't search for nothing...")
-        return
     try:
-        manga = client.search_manga(query)
-    except APIException as e:
-        refresh_token(msg, e)
-    if not manga:
-        msg.reply_text("Not found!")
-        return
-    manga_id = manga[0].id
-    res = client.get_manga_details(manga_id)
-    genres_list = []
-    for i in res.genres:
-        genres_list.append(i.name)
-    genres = ", ".join(genres_list)
-    image = res.main_picture.large
+        search = jikan.search("character", query).get("results")[0].get("mal_id")
+    except APIException:
+        msg.reply_text("No results found!")
+        return ""
+    if search:
+        try:
+            res = jikan.character(search)
+        except APIException:
+            msg.reply_text("Error connecting to the API. Please try again!")
+            return ""
+    if res:
+        name = res.get("name")
+        kanji = res.get("name_kanji")
+        about = res.get("about")
+        if len(about) > 4096:
+            about = about[:4000] + "..."
+        image = res.get("image_url")
+        url = res.get("url")
+        rep = f"<b>{name} ({kanji})</b>\n\n"
+        rep += f"<a href='{image}'>\u200c</a>"
+        rep += f"<i>{about}</i>\n"
+        keyb = [
+            [InlineKeyboardButton("More Information", url=url)]
+        ]
+        
+        msg.reply_text(rep, parse_mode=ParseMode.HTML, reply_markup=InlineKeyboardMarkup(keyb))
+        
+        
+@run_async
+def upcoming(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    rep = "<b>Upcoming anime</b>\n"
+    later = jikan.season_later()
+    anime = later.get("anime")
+    for new in anime:
+        name = new.get("title")
+        url = new.get("url")
+        rep += f"• <a href='{url}'>{name}</a>\n"
+        if len(rep) > 2000:
+            break
+    msg.reply_text(rep, parse_mode=ParseMode.HTML)
     
-    text = f"<b>{res.title} ({res.alternative_titles.ja})</b>\n"
-    text += f"<b>Type</b>: <code>{res.media_type.capitalize()}</code>\n"
-    text += f"<b>Status</b>: <code>{res.status.replace('_', ' ').capitalize()}</code>\n"
-    text += f"<b>Genres</b>: <code>{genres}</code>\n"
-    text += f"<b>Score</b>: <code>{res.mean}</code>\n"
-    text += f"<b>Ranked</b>: <code>#{res.rank}</code>\n"
-    if res.num_volumes:
-        text += f"<b>Volumes</b>: <code>{res.num_volumes}</code>\n"
-    if res.num_chapters:
-        text += f"<b>Chapters</b>: <code>{res.num_chapters}</code>\n"
-    text += f"<a href='{image}'>\u200c</a>"
-    text += f"\n{res.synopsis}"
-    keyb = [
-        [InlineKeyboardButton("More Information", url=f"https://myanimelist.net/manga/{manga_id}")]
-    ]
     
-    msg.reply_text(text, parse_mode="HTML", reply_markup=InlineKeyboardMarkup(keyb))
-
-
+@run_async
+def manga(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    args = context.args
+    query = " ".join(args)
+    res = ""
+    manga = ""
+    try:
+        res = jikan.search("manga", query).get("results")[0].get("mal_id")
+    except APIException:
+        msg.reply_text("Error connecting to the API. Please try again!")
+        return ""
+    if res:
+        try:
+            manga = jikan.manga(res)
+        except APIException:
+            msg.reply_text("Error connecting to the API. Please try again!")
+            return ""
+        title = manga.get("title")
+        japanese = manga.get("title_japanese")
+        type = manga.get("type")
+        status = manga.get("status")
+        score = manga.get("score")
+        volumes = manga.get("volumes")
+        chapters = manga.get("chapters")
+        genre_lst = manga.get("genres")
+        genres = ""
+        for genre in genre_lst:
+            genres += genre.get("name") + ", "
+        genres = genres[:-2]
+        synopsis = manga.get("synopsis")
+        image = manga.get("image_url")
+        url = manga.get("url")
+        rep = f"<b>{title} ({japanese})</b>\n"
+        rep += f"<b>Type:</b> <code>{type}</code>\n"
+        rep += f"<b>Status:</b> <code>{status}</code>\n"
+        rep += f"<b>Genres:</b> <code>{genres}</code>\n"
+        rep += f"<b>Score:</b> <code>{score}</code>\n"
+        rep += f"<b>Volumes:</b> <code>{volumes}</code>\n"
+        rep += f"<b>Chapters:</b> <code>{chapters}</code>\n\n"
+        rep += f"<a href='{image}'>\u200c</a>"
+        rep += f"<i>{synopsis}</i>"
+        keyb = [
+            [InlineKeyboardButton("More Information", url=url)]
+        ]
+        
+        msg.reply_text(rep, parse_mode=ParseMode.HTML, reply_markup=InlineKeyboardMarkup(keyb))
+        
+        
 __help__ = """
-Get information about anime and manga with the help of this module! All data is fetched from [MyAnimeList](https://myanimelist.net).
+Get information about anime, manga or characters with the help of this module! All data is fetched from [MyAnimeList](https://myanimelist.net).
 *Available commands:*
- - /anime <anime>: returns information about the anime.
- - /manga <manga>: returns information about the manga.
+ - /manime <anime>: returns information about the anime.
+ - /mcharacter <character>: returns information about the character.
+ - /mmanga <manga>: returns information about the manga.
+ - /mupcoming: returns a list of new anime in the upcoming seasons.
  """
 
-
 __mod_name__ = "MyAnimeList"
-
-
-ANIME_HANDLER = DisableAbleCommandHandler("anime", search_anime, pass_args=True)
-MANGA_HANDLER = DisableAbleCommandHandler("manga", search_manga, pass_args=True)
+        
+        
+ANIME_HANDLER = CommandHandler("manime", anime, pass_args=True)
+CHARACTER_HANDLER = CommandHandler("mcharacter", character, pass_args=True)
+UPCOMING_HANDLER = CommandHandler("mupcoming", upcoming)
+MANGA_HANDLER = CommandHandler("mmanga", manga, pass_args=True)
 
 dispatcher.add_handler(ANIME_HANDLER)
+dispatcher.add_handler(CHARACTER_HANDLER)
+dispatcher.add_handler(UPCOMING_HANDLER)
 dispatcher.add_handler(MANGA_HANDLER)
