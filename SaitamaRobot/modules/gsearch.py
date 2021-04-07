8ff7afc23e524d454a1c07aa66ca4458e11d1cf5
Tanjiro Kamado <69890535+JayPatel1314@users.noreply.github.com>
2021-04-07 11:33:22 +0530

    Update gsearch.py

--- SaitamaRobot/modules/gsearch.py
+++ SaitamaRobot/modules/gsearch.py
@@ -12,15 +12,15 @@ async def is_register_admin(chat, user):
 
         return isinstance(
             (
-                await client(functions.channels.GetParticipantRequest(chat, user))
+                await telethn(functions.channels.GetParticipantRequest(chat, user))
             ).participant,
             (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
         )
     elif isinstance(chat, types.InputPeerChat):
 
-        ui = await client.get_peer_id(user)
+        ui = await telethn.get_peer_id(user)
         ps = (
-            await client(functions.messages.GetFullChatRequest(chat.chat_id))
+            await telethn(functions.messages.GetFullChatRequest(chat.chat_id))
         ).full_chat.participants.participants
         return isinstance(
             next((p for p in ps if p.user_id == ui), None),
