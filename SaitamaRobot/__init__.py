5d81c79d70a33612f1e2adb688a77872a259b96d
Tanjiro Kamado <69890535+JayPatel1314@users.noreply.github.com>
2021-04-21 10:06:10 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -7,7 +7,6 @@ import spamwatch
 import telegram.ext as tg
 from redis import StrictRedis
 from telethon import TelegramClient
-from pyrogram import Client, errors
 
 StartTime = time.time()
 
@@ -199,7 +198,6 @@ else:
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 oko = TelegramClient("TanjiroKamado", API_ID, API_HASH)
 telethn = TelegramClient("tanjiro", API_ID, API_HASH)
-pgram = Client("TanjiroPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
